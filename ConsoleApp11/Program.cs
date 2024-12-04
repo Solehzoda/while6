5cfd0a90145040129f02c9c71929bd360d3be556
Solehzoda <salikhovajillo@gmail.com>
2024-12-04 17:53:57 +0500

    первый коммит

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            int i = 1;
+            while (n >= 1)
+            {
+                i *= 0;
+                n -= 2;
+
+            }
+            Console.WriteLine($"Двойной факториал равен {i}");
+        }
+    }
+}
